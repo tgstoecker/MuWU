d7baaab0acdad1b89c3f070a295852d100cd389d
Tyll Stoecker <stoecker@cs03.cropbio.uni-bonn.de>
2021-06-22 14:54:50 +0000

    user gets shown the RelocaTE2 found karma TEs

--- other_TE_tests/MuWU_RelocaTE2_test_with_karma_TE/input/karma_quick_overlap_check.R
+++ other_TE_tests/MuWU_RelocaTE2_test_with_karma_TE/input/karma_quick_overlap_check.R
@@ -3,17 +3,21 @@ library(IRanges)
 
 
 rTE2_karma <- read.csv("input/karma_TE_relocaTE2_results.gff", header=FALSE, sep="\t")
-rTE2_karma <- rTE2_karma[, c(1,4,5)]
-names(rTE2_karma) <- c("Chr" , "Start", "End")
+rTE2_karma_short <- rTE2_karma[, c(1,4,5)]
+names(rTE2_karma_short) <- c("Chr" , "Start", "End")
 
 
 MuWU_karma <- read.csv("results/insertions_table_final/all_identified_insertions.csv", header=TRUE, sep=",")
 
 
-message("RelocaTE2 finds 23 karma insertions in this dataset.")
+message("RelocaTE2 finds 23 karma insertions in this dataset:")
+message("")
+rTE2_karma
+
+message("")
 message("Overlapping with MuWU results:")
 message("")
-joined_sets <- fuzzyjoin::genome_join(rTE2_karma, MuWU_karma, by=c("Chr", "Start", "End"), mode="inner")
+joined_sets <- fuzzyjoin::genome_join(rTE2_karma_short, MuWU_karma, by=c("Chr", "Start", "End"), mode="inner")
 joined_sets
 
 message("")
